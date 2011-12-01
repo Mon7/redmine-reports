bf9a2818a2709bb2022f979f6d4e35578f1df62a
Magnus Hörberg <magnus.hoerberg@gmail.com>
2011-12-01 15:17:59 +0100

    la till namn variabel

--- lib/home.rb
+++ lib/home.rb
@@ -10,17 +10,23 @@ class Home < Base
   
   get '/' do
     @projects = redmine.projects['projects']
+    p @projects
+    p 'done'
     haml :index
   end
   get '/statistics' do
     @result = redmine.time_entries(params['period'])
     haml :stat
   end
+  get '/statistics/:name' do |name|
+    redmine.time_entries(name, params['period']).to_json
+  end
 
   get '/:name.pdf' do |name|
     content_type 'application/pdf'
     response['Content-Disposition'] = "inline; filename=#{name}_#{Date.today.strftime("%B")}.pdf"
     @project = redmine.get_project(name, params[:period])['time_entries'].compact
+    @name = @project.first['project']['name']
     @issues = redmine.get_issues(name)['issues']
     pdf(:debiteringsunderlag)
   end
