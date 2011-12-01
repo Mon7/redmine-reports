45e1b44e23295d05f326e0d7bb6e75b70f7f4812
Magnus Hörberg <magnus.hoerberg@gmail.com>
2011-12-01 13:44:58 +0100

    fixa så alla issuse kommmer med

--- lib/redmine.rb
+++ lib/redmine.rb
@@ -36,8 +36,15 @@ class Redmine
     res
   end
   def get_issues name
-    url = "/projects/#{name}/issues.json?limit=100"
-    self.class.get url
+    url = "/projects/#{name}/issues.json?limit=100&set_filter=1&f%5B%5D=status_id&op%5Bstatus_id%5D=*&f%5B%5D=&c%5B%5D=tracker&c%5B%5D=status&c%5B%5D=priority&c%5B%5D=subject&c%5B%5D=assigned_to&c%5B%5D=updated_on&group_by="
+    res = self.class.get url
+    count = res['total_count'].to_i
+    nr_of_time_entries = ((count - 100)/100.0).ceil
+    nr_of_time_entries.times do |i|
+      url = "/projects/#{name}/time_entries.json?period_type=1&period=#{period}&limit=100&offset=#{100 * (i + 1)}"
+      res['time_entries'] += self.class.get(url)['time_entries']
+    end
+    res
   end
 
   def projects
